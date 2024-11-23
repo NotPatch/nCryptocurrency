9ba54ad8121a45148cf002b070ae5843b4060fc1
Nitra <nitradosamet@hotmail.com>
2024-11-23 23:25:15 +0300

    Fixed

--- src/main/java/com/notpatch/NCryptocurrency.java
+++ src/main/java/com/notpatch/NCryptocurrency.java
@@ -3,13 +3,8 @@ package com.notpatch;
 import com.notpatch.markets.BinanceAPI;
 import com.notpatch.markets.CoinGeckoAPI;
 import com.notpatch.markets.CryptoAPI;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class NCryptocurrency {
-
-    private static JavaPlugin plugin;
-
-    private static final NCryptocurrency INSTANCE = new NCryptocurrency();
 
     private static final CryptoAPI coinGeckoAPI = new CoinGeckoAPI();
     private static final CryptoAPI binanceAPI = new BinanceAPI();
@@ -23,21 +18,6 @@ public class NCryptocurrency {
 
     public static double getBinancePrice(String crypto, String currency) {
         return binanceAPI.getPrice(crypto, currency);
-    }
-
-
-    public static void setPlugin(final JavaPlugin plugin) {
-        if (NCryptocurrency.plugin != null) {
-            throw new IllegalStateException("You already referenced api!");
-        }
-        NCryptocurrency.plugin = plugin;
-    }
-
-    public static NCryptocurrency getAPI() {
-        if (plugin == null) {
-            throw new IllegalStateException("Plugin reference not set! Call setPlugin() first.");
-        }
-        return INSTANCE;
     }
 
 }
